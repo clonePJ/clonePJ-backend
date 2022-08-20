2c20e060298435f72529adeb74ee917b1bb3ed77
minyoung117 <bree.j117@gmail.com>
2022-08-20 22:11:42 +0900

    코드.. 짜집기완료

---
+++ src/layers/repositories/user.repository.js
@@ -0,0 +1,18 @@
+const { Users } = require('../models')
+class UserRepository {
+  checkEmailDup = async (email) => {
+    return await Users.findOnd({ where: { email } });
+  }
+  checkNicknameDup = async (nickname) => {
+    return await Users.findOne({ where: { nickname } });
+  }
+  createUser = async (email, nickname, password) => {
+    await Users.create({ email, nickname, password });
+  }
+  checkUserDup = async (email) => {
+    const data = await Users.findOne({ where: { email }, raw: true });
+    return data;
+  }
+
+}
+module.exports = UserRepository;
--- src/layers/routers/user.router.js
+++ src/layers/routers/user.router.js
@@ -1,3 +1,11 @@
-const { Router } = require('express');
+const express = require('express');
+const router = express.Router;
+
+const UserController = require('../controllers/user.controller');
+const userController = new UserController();
+const signMiddleware = require('../middlewares/sign.middleware')
+
+router.use(signMiddleware);
+router.post('/dup/email')
 const userRouter = Router();
 module.exports = userRouter;
