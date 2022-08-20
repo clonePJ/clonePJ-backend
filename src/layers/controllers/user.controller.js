f23e1f1796a32c14fae2fa940320e90c3d828245
minyoung117 <bree.j117@gmail.com>
2022-08-20 13:37:00 +0900

    user controller만 완, 나머지 미완 test commit

---
+++ src/layers/controllers/user.controller.js
@@ -0,0 +1,37 @@
+const bcrypt = require("bcrypt");
+const Userservice = require("../services/user.service");
+
+class UserController {
+  userService = new Userservice();
+
+  checkEmailDup = async (req, res, next) => {
+    const { email } = req.body;
+    try {
+      const result = await this.userService.checkEmailDup(email);
+      return res.status(200).json({ result });
+    } catch (err) {
+      return res.status(400).json({ result: err.message });
+
+    }
+  }
+  signup = async (req, res, next) => {
+    const { email, nickname, password } = req.body;
+    try {
+      await this.userService.signup(email, nickname, password);
+      res.status(200).json({ result: true });
+    } catch (err) {
+      res.status(400).json({ result: false });
+    }
+  };
+  signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      const gettoken = await this.userService.signin(email, password);
+      res.status(200).json({ result: true, token: gettoken });
+    } catch (err) {
+      res.status(400).json({ result: false });
+    }
+  };
+
+}
+module.exports = UserController
