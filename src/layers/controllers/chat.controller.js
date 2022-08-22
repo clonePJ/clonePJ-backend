7c1173201a1f9e076903bd3a51db6651c9ed11b5
SoularOfDawn <tntkala001@gmail.com>
2022-08-22 22:12:57 +0900

    Update Chat Using Room Repo

--- src/layers/controllers/chat.controller.js
+++ src/layers/controllers/chat.controller.js
@@ -4,15 +4,9 @@ module.exports = class ChatController {
     chatService = new ChatService();
 
     sendResponse = (res, status, success, extra) => {
-        console.log(status, success);
-        console.log(extra ? true : false, extra);
-        if (extra && success) {
-            return res.status(status).json({ success, data: extra });
-        } else if (extra) {
-            return res.status(status).json({ success, message: extra });
-        } else {
-            return res.status(status).json({ success });
-        }
+        if (extra && success) return res.status(status).json({ success, data: extra });
+        else if (extra) return res.status(status).json({ success, message: extra });
+        else return res.status(status).json({ success });
     };
 
     createNewChat = async (req, res) => {
@@ -28,58 +22,74 @@ module.exports = class ChatController {
                     content: joi.string().required(),
                 })
                 .validateAsync({ roomId, userId, content });
-            const response = await this.chatService.createChat(roomId, userId, content);
-
-            const result = await this.roomService.postRoom(roomName, category, ownerUserId);
-            return res.status(201).json({ status: 200, success: true, result: result });
         } catch (err) {
-            console.log(err);
-            return res
-                .status(400)
-                .json({ status: 400, success: false, message: '입력한 형식이 맞지 않습니다.' });
+            return this.sendResponse(res, 400, false, '입력한 형식이 맞지 않습니다.');
         }
 
-        console.log(response);
+        const response = await this.chatService.createChat(roomId, userId, content);
         const r = response;
-        console.log(r);
         return this.sendResponse(res, r.status, r.success, r.success ? null : r.message);
     };
 
     findRoomChatList = async (req, res) => {
         const { roomId } = req.params;
-        // const { userId } = res.locals;
-        const userId = 1;
+        const { userId } = res.locals;
+
+        try {
+            await joi
+                .object({
+                    roomId: joi.number().required(),
+                    userId: joi.number().required(),
+                })
+                .validateAsync({ roomId, userId });
+        } catch (err) {
+            return this.sendResponse(res, 400, false, '입력한 형식이 맞지 않습니다.');
+        }
 
         const response = await this.chatService.findRoomChat(roomId, userId);
-        console.log(response);
         const r = response;
-        console.log(r);
         return this.sendResponse(res, r.status, r.success, r.success ? r.data : r.message);
     };
 
     updateChat = async (req, res) => {
-        // const { userId } = res.locals;
-        const userId = 1;
-        const chatId = req.params;
+        const { chatId } = req.params;
+        const { userId } = res.locals;
         const { content } = req.body;
 
+        try {
+            await joi
+                .object({
+                    chatId: joi.number().required(),
+                    userId: joi.number().required(),
+                    content: joi.string().required(),
+                })
+                .validateAsync({ chatId, userId, content });
+        } catch (err) {
+            return this.sendResponse(res, 400, false, '입력한 형식이 맞지 않습니다.');
+        }
+
         const response = await this.chatService.updateChat(chatId, userId, content);
-        console.log(response);
         const r = response;
-        console.log(r);
         return this.sendResponse(res, r.status, r.success, r.success ? null : r.message);
     };
 
     deleteChat = async (req, res) => {
-        // const { userId } = res.locals;
-        const userId = 1;
-        const chatId = req.params;
-        const { content } = req.body;
+        const { chatId } = req.params;
+        const { userId } = res.locals;
+
+        try {
+            await joi
+                .object({
+                    chatId: joi.number().required(),
+                    userId: joi.number().required(),
+                })
+                .validateAsync({ roomId, userId });
+        } catch (err) {
+            return this.sendResponse(res, 400, false, '입력한 형식이 맞지 않습니다.');
+        }
 
         const response = await this.chatService.deleteChat(chatId, userId);
-        console.log(response);
         const r = response;
-        console.log(r);
         return this.sendResponse(res, r.status, r.success, r.success ? null : r.message);
     };
 };
