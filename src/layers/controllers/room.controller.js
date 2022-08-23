a6e93bfe3284e73696d7fd72aca6d9af1fa07144
waveinyu <waveinyu@gmail.com>
2022-08-23 21:01:39 +0900

    Modify detail

--- src/layers/controllers/room.controller.js
+++ src/layers/controllers/room.controller.js
@@ -34,29 +34,20 @@ class RoomController {
             );
             return res.status(201).json({ status: 200, success: true, result: result });
         } catch (err) {
-            console.log(err);
             return res
                 .status(400)
                 .json({ status: 400, success: false, message: '입력한 형식이 맞지 않습니다.' });
-            throw err;
         }
     };
 
     getRoom = async (req, res, next) => {
         const { category } = req.query;
-        try {
-            if (category === '') {
-                const getAllRoom = await this.roomService.getRoom();
-                return res.status(200).json({ status: 200, success: true, result: getAllRoom });
-            } else {
-                const getCategoryRoom = await this.roomService.getCategoryRoom(category);
-                return res
-                    .status(200)
-                    .json({ status: 200, success: true, result: getCategoryRoom });
-            }
-        } catch (err) {
-            console.log(err);
-            throw err;
+        if (category === '') {
+            const getAllRoom = await this.roomService.getRoom();
+            return res.status(200).json({ status: 200, success: true, result: getAllRoom });
+        } else {
+            const getCategoryRoom = await this.roomService.getCategoryRoom(category);
+            return res.status(200).json({ status: 200, success: true, result: getCategoryRoom });
         }
     };
 
@@ -70,7 +61,6 @@ class RoomController {
         else {
             return res.status(400).json({ result: result });
         }
-
     };
 }
 
