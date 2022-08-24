c444baa26e4b4b05e05583f2c8e4dd8949090644
SoularOfDawn <tntkala001@gmail.com>
2022-08-25 00:01:32 +0900

    v1.0.0

--- src/layers/routers/user.router.js
+++ src/layers/routers/user.router.js
@@ -6,11 +6,8 @@ const userController = new UserController();
 
 const rejectMiddleware = require('../middlewares/reject.middleware');
 const authMiddleware = require('../middlewares/auth.middleware');
-//const signMiddleware = require('../middlewares/auth.middleware')
 
-//router.use(signMiddleware);
 router.post('/signup', rejectMiddleware, userController.signup);
-router.post('/test', rejectMiddleware);
 router.post('/login', rejectMiddleware, userController.signin);
 router.delete('/quit', authMiddleware, userController.quit);
 module.exports = router;
